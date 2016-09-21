1ef16c407253bf1c6239851f6eff388e38395c7d
fanyongdong daben <fanyongdong@niu.com>
2016-09-21 20:59:07 +0800

    update

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 export STATIC_PORT=3000
 export MOCK_PORT=3002
-
+SHELL_PATH=$(cd `dirname $0`; pwd)
 build_local() {
  node_modules/gulp/bin/gulp.js
 }
 build_dev() {
   node_modules/gulp/bin/gulp.js aglio
 }
-
+cd $SHELL_PATH
 MODE=${1:-"local"}
 "build_${MODE}";
 
